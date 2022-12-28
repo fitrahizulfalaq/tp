6dcf7be30745ce177da5d32234c08d00b3e3cbad
uun <uunsetiawati@gmail.com>
2022-12-28 16:27:26 +0700

    tampilan target data

--- application/views/kunjungan/kunjungan_input.php
+++ application/views/kunjungan/kunjungan_input.php
@@ -6,24 +6,25 @@
         <h1 class="mb-0"><?= date('d / M / y')?></h1>
         <h4 class="mb-0">Input Kunjungan Hari Ini </h4>
     </div>
-    <div class="section mt-2 mb-5">
-        <form action="<?=base_url("kunjungan/addkunjungan")?>" enctype="multipart/form-data" method="post">
+    <hr>    
 
-            <div class="form-group boxed">
-                <div class="input-wrapper">
-                    <label class="text" for="name3">Kunjungan <span class="error">*</span></label>
-                      
-                      <select name="tipe" class="form-control" onchange="ukmkoperasi()" id="pilihtipe">
-                        <option value="">--PILIH KUNJUNGAN--</option>
-                        <option value="UKM" >UKM</option>
-                        <option value="KOPERASI" >KOPERASI</option>
-                        <option value="CALON WIRAUSAHA" >CALON WIRAUSAHA</option>
-                        <option value="LAINNYA" >LAINNYA</option>
-                      </select>
-                </div>
+    <div class="section mt-2 mb-5" >
+        <div class="form-group boxed">
+            <div class="input-wrapper">
+                <label class="label" for="name3">Kunjungan <span class="error">*</span></label>
+                    
+                    <select name="tipe" class="form-control" onchange="ukmkoperasi()" id="pilihtipe">
+                    <option value="">--PILIH KUNJUNGAN--</option>
+                    <option value="UKM" >UKM</option>
+                    <option value="KOPERASI" >KOPERASI</option>
+                    <option value="CALON WIRAUSAHA" >CALON WIRAUSAHA</option>
+                    <option value="LAINNYA" >LAINNYA</option>
+                    </select>
             </div>
-            
-            <div class="form-group boxed" id="ukm" style="visibility: hidden;">
+        </div>
+        <hr>
+        <form action="<?=base_url("")?>" enctype="multipart/form-data" method="post" id="ukm" style="visibility: hidden;">               
+            <div class="form-group boxed" >
                 <div class="input-wrapper">                                     
                     <label class="label" for="name3">Nama <span id="alert-resume" class="error">*</span></label>
                     <input type="text" id="word" name="nama" class="form-control" placeholder="Nama Pelaku Usaha/Koperasi yang dikunjungi" required>
@@ -60,22 +61,8 @@
                     </i>                    
                 </div>
 
-            </div>
-
-            <div class="form-group boxed" id="lainnya" style="visibility: hidden;">
-                <div class="input-wrapper">                                     
-                    <label class="label" for="name3">Permasalahan yang dihadapi <span id="alert-resume" class="error">*</span></label>
-                    <input type="text" id="word" name="masalah" class="form-control" placeholder="Cth. " required>
-                    <i class="clear-input">
-                        <ion-icon name="close-circle"></ion-icon>
-                    </i>                    
-                </div>
-            </div>
-            
-
-            
-            </br>
-            </br>
+            </div>            
+            <hr>
 
             <input type="hidden" value="<?= $lat?>" name="lat">
             <input type="hidden" value="<?= $lng?>" name="lng">
@@ -84,7 +71,56 @@
             <div class="form-button">
                 <button type="submit" onclick="return checkWordCount()" class="btn btn-success btn-block btn-lg">KIRIM</button>
             </div>
+        </form>
 
+        <form action="<?=base_url("")?>" enctype="multipart/form-data" method="post" id="lainnya" style="visibility: hidden;">               
+            <div class="form-group boxed" >
+                <div class="input-wrapper">                                     
+                    <label class="label" for="name3">Nama <span id="alert-resume" class="error">*</span></label>
+                    <input type="text" id="word" name="nama" class="form-control" placeholder="Nama Pelaku Usaha/Koperasi yang dikunjungi" required>
+                    <i class="clear-input">
+                        <ion-icon name="close-circle"></ion-icon>
+                    </i>                    
+                </div>
+                <hr>
+                <div class="input-wrapper">                                     
+                    <label class="label" for="name3">Jenis Kelamin <span id="alert-resume" class="error">*</span></label>                   
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons" required>
+                        <label class="btn btn-outline-primary">
+                            <input type="radio" name="jk" id="option2" value="Laki-Laki"> Laki-Laki
+                        </label>
+                        <label class="btn btn-outline-primary">
+                            <input type="radio" name="jk" id="option3" value="Perempuan"> Perempuan
+                        </label>
+                    </div>
+                </div>
+                <hr> 
+                <div class="input-wrapper">                                     
+                    <label class="label" for="name3">No. Telepon (WhatsApp) <span id="alert-resume" class="error">*</span></label>
+                    <input type="tel" id="word" name="hp" class="form-control" placeholder="No. Telp/WhatsApp Pelaku Usaha/Koperasi yang dikunjungi" required>
+                    <i class="clear-input">
+                        <ion-icon name="close-circle"></ion-icon>
+                    </i>                    
+                </div>
+                <hr>
+                <div class="input-wrapper">                                     
+                    <label class="label" for="name3">Tujuan <span id="alert-resume" class="error">*</span></label>
+                    <input type="text" id="word" name="tujuan" class="form-control" placeholder="Tujuan Kunjungan" required>
+                    <i class="clear-input">
+                        <ion-icon name="close-circle"></ion-icon>
+                    </i>                    
+                </div>
+
+            </div>            
+            <hr>
+
+            <input type="hidden" value="<?= $lat?>" name="lat">
+            <input type="hidden" value="<?= $lng?>" name="lng">
+            <input type="hidden" value="<?= $loc_img?>" name="loc_img">
+
+            <div class="form-button">
+                <button type="submit" onclick="return checkWordCount()" class="btn btn-success btn-block btn-lg">KIRIM</button>
+            </div>
         </form>
     </div> 
 
--- application/views/target/target_data.php
+++ application/views/target/target_data.php
@@ -1,79 +1,38 @@
 <!-- App Capsule -->
 <div id="appCapsule">
 
-
-<div class="section full mt-2">
-    
+<div class="section full mt-2">    
     
     <div class="row">
 		<div class="col-12">
 			<div class="col-12">	
-            <h3>DATA TARGET</h3>
-            <div class="card">            
-				<div class="card-body">
-                    
-				
-				<form action="" enctype="multipart/form-data" method="post" accept-charset="utf-8">
-                
-					<input type="hidden" name="id" value="<?= $this->session->id ?>">
-                    
-					<div class="input-group mb-3">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fa fa-calendar"></i></span>
-						</div>
-
-						<select name="tahun" class="btn btn-outline-primary " required>
-							<option value="">Pilih Tahun</option>
-							<?php $thn_skr = date('Y');
-							for ($x = $thn_skr; $x >= 2018; $x--) { ?>
-								<option value="<?php echo $x ?>"><?php echo $x ?></option>
-							<?php } ?>
-						</select>
-                        
-
-                        
-						<button type="submit" class="btn btn-primary">Filter <i class="fa fa-eye"></i></button>
-                        
-					</div>
-                    
-					<div class="input-group-append">
-					</div>
-				</form>
-                            </div>
-                            </div>
-                            <br>
+            	<h3>DATA TARGET</h3>            
 				<div class="card">
 					<div class="card-body">
 						<div class="table-responsive">
-                        <div style="overflow-x:auto;">
-							<table class="<table table-striped table-bordered table-hover table-full-width dataTable" id="list">
-								<thead>
-									<tr>
-										<th width="5%">No</th>
-										<th width="40%">Tahun</th>
-                                        <th width="40%">Target</th>
-                                        <th width="30%">#</th>
+                        	<div style="overflow-x:auto;">
+								<table class="<table table-striped table-bordered table-hover table-full-width dataTable" id="list">
+									<thead>
+										<tr>
+											<th width="40%">Tahun</th>
+											<th width="30%">Aksi</th>
 
-									</tr>
-								</thead>
-								<tbody>
-                                    <tr>
-                                        <td scope="row">
-                                            <p>1</p>
-                                        </td>
-                                        <td scope="row">
-                                            <p>2023</p>                                        
-                                        </td>
-                                        <td scope="row">
-                                            <p>file pdf </p>
-                                        </td>                                        
-                                        <td>
-                                            <a href="" class="btn btn-danger"><i class="fa fa-download"></i></a>
-                                            <a href="" class="btn btn-warning"><i class="fa fa-print"></i></a>
-                                        </td>
-                                    </tr>									
-								</tbody>
-							</table>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											</td>
+											<td scope="row">
+												<p>2023</p>                                        
+											</td>    
+											<td>
+												<a href="" class="btn btn-primary"><i class="fa fa-eye"></i></a>
+												<a href="" class="btn btn-danger"><i class="fa fa-download"></i></a>
+												<a href="" class="btn btn-warning"><i class="fa fa-print"></i></a>
+											</td>
+										</tr>									
+									</tbody>
+								</table>
                             </div>
 						</div>
 					</div>
