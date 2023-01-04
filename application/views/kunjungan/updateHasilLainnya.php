bd9db35aa5fcfbb3b2cbde98809b323f09911995
uun <uunsetiawati@gmail.com>
2023-01-04 13:09:07 +0700

    edit tampilan target dan lokasi

--- application/views/kunjungan/updateHasilLainnya.php
+++ application/views/kunjungan/updateHasilLainnya.php
@@ -22,7 +22,7 @@
                 <hr>
                 <div class="input-wrapper">
                     <label class="label">Target <span id="alert-resume" class="error">*</span></label>
-                    <input type="tel" id="word" name="target" class="form-control" placeholder="Target yang ingin dicapai" required minlength="10" value="<?= $row->target?>">
+                    <input type="text" id="word" name="target" class="form-control" placeholder="Target yang ingin dicapai" required minlength="10" value="<?= $row->target?>">
                     <i class="clear-input">
                         <ion-icon name="close-circle"></ion-icon>
                     </i>
