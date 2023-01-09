ce52ada2816afd88b54ba3bbedeace966dc7e93f
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-01-09 19:44:30 +0700

    tetep sha1 aja

--- application/views/pengaturan/setDevice.php
+++ application/views/pengaturan/setDevice.php
@@ -10,7 +10,7 @@
             <p>Kuota Device Tersedia <?= 5 - $row->num_rows() ?></p>
             <?php $this->view("message")?>
             <br>
-            <input type="text" value="Token : <?= md5($this->agent->agent_string()) ?>" class="form-control" readonly>
+            <input type="text" value="Token : <?= sha1($this->agent->agent_string()) ?>" class="form-control" readonly>
             <br>
             <input type="text" value="Platform : <?= $this->agent->platform() ?>" class="form-control" readonly>
             <br>
@@ -43,7 +43,7 @@
                                 <p><?= $data->platform ?></p>
                             </td>
                             <td>
-                                <?= md5($data->token) ?>
+                                <?= sha1($data->token) ?>
                             </td>
                             <td>
                                 <?= $data->browser ?>
