a8168047b52847fdef0ac173497ff19ab2504ef5
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-01-11 19:14:06 +0700

    update notify wa

--- application/controllers/Notify.php
+++ application/controllers/Notify.php
@@ -1,9 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
- 
 class Notify extends CI_Controller {
- 
-    function __construct(){
+
+function __construct(){
         parent::__construct();
         $this->load->library("wa");
     }
@@ -21,13 +20,45 @@ class Notify extends CI_Controller {
         foreach ($dataUser->result() as $key => $data) {
             $dataLogin = $this->kunjungan_m->getByDate(date("Y"),date("m"),date("d"),$data->id);
             if ($dataLogin->num_rows() == null) {
-                $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nTerhitung pada ".date("d-m-Y H:i:s")." kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\nProvide by TIM IT UPT";
+                $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nHalo, ".$data->nama." Terhitung pada ".date("d-m-Y H:i:s")." kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\n_Sistem Otomatis oleh TIM IT UPT_";
+                echo $kalimat."<br>";
                 $this->wa->send($data->hp,$kalimat);
+                sleep(4); // this should halt for 3 seconds for every loop
+                ob_flush();
+                flush();            
             }
-            sleep(9); // this should halt for 3 seconds for every loop
-            ob_flush();
-            flush();            
         }
+    }
+
+    function jarak() {
+        $point1 = array("lat" => -7.8763715, "long" => 112.5195452);
+        $point2 = array("lat" => -7.866504, "long" => 112.525668);
+
+        $lat1 = $point1['lat'];
+        $lon1 = $point1['long'];
+        $lat2 = $point2['lat'];
+        $lon2 = $point2['long'];
+
+        $theta = $lon1 - $lon2;
+        $miles = (sin(deg2rad($lat1)) * sin(deg2rad($lat2))) + (cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta)));
+        $miles = acos($miles);
+        $miles = rad2deg($miles);
+        $miles = $miles * 60 * 1.1515;
+        $feet  = $miles * 5280;
+        $yards = $feet / 3;
+        $kilometers = $miles * 1.609344;
+        $meters = $kilometers * 1000;
+        $str = compact('miles','feet','yards','kilometers','meters');
+
+        test($str);
+    }
+
+    function getJarak()
+    {
+        $this->load->model("validation_m");
+        $data = $this->validation_m->getLocationByAddress("Jl KH. Abd. Hamid Gg. No. 943, Kota Probolinggo");
+        $data = $data['results']['0']['geometry']['location'];
+        var_dump($data['lat']);
     }
     
 }
--- application/models/Validation_m.php
+++ application/models/Validation_m.php
@@ -4,6 +4,7 @@
     https://ceo.bikinkarya.com
     081231390340
 */
+use GuzzleHttp\Client;
 
 class Validation_m extends CI_Model
 {
@@ -135,6 +136,41 @@ class Validation_m extends CI_Model
 		$this->db->order_by("created","DESC");
 		$query = $this->db->get();
 		return $query; 
+	}
+
+	function getLocationByAddress($address)
+	{
+		$client = new Client();
+		$response = $client->request('GET','https://maps.googleapis.com/maps/api/geocode/json',[
+			'query' => [
+				'key' => 'AIzaSyBulTatyUv6oR6ykvWU-QDzp-wYQXNWV7A',
+				'address' => $address
+			]
+		]);
+		$result = json_decode($response->getBody()->getContents(),true);
+		return $result;
+	}
+
+	function hitungJarak($lat1,$lng1,$lat2,$lng2)
+	{
+		$point1 = array("lat" => $lat1, "long" => $lng1);
+        $point2 = array("lat" => $lat1, "long" => $lng2);
+
+        $lat1 = $point1['lat'];
+        $lon1 = $point1['long'];
+        $lat2 = $point2['lat'];
+        $lon2 = $point2['long'];
+
+        $theta = $lon1 - $lon2;
+        $miles = (sin(deg2rad($lat1)) * sin(deg2rad($lat2))) + (cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta)));
+        $miles = acos($miles);
+        $miles = rad2deg($miles);
+        $miles = $miles * 60 * 1.1515;
+        $feet  = $miles * 5280;
+        $yards = $feet / 3;
+        $kilometers = $miles * 1.609344;
+        $meters = $kilometers * 1000;
+        return compact('miles','feet','yards','kilometers','meters');
 	}
 
 }
