e994121ba6be5bb6588a581abe9fbed964328d6d
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-01-12 19:24:08 +0700

    notiy belum checkin

--- application/controllers/Notify.php
+++ application/controllers/Notify.php
@@ -21,13 +21,25 @@ function __construct(){
             $dataLogin = $this->kunjungan_m->getByDate(date("Y"),date("m"),date("d"),$data->id);
             $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin","user_id",$data->id,date("Y-m-d"));
             if ($dataLogin->num_rows() == null and $dataIzin->num_rows() == null) {
-                // $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nHalo, ".$data->nama.$data->hp." Terhitung pada ".date("d-m-Y H:i:s")." kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\n_Sistem Otomatis oleh TIM IT UPT_";
-                // echo $kalimat."<br>";
                 $this->kunjungan_m->saveLate($data->id,"telat check in");
-                // $this->wa->send($data->hp,$kalimat);
-                // sleep(4); // this should halt for 3 seconds for every loop
-                // ob_flush();
-                // flush();            
+            }
+        }
+    }
+
+    function belumCheckin()
+    {
+        $this->load->model("user_m");
+        $this->load->model("kunjungan_m");
+        $dataUser = $this->user_m->getAllby("tipe_user","1");
+        foreach ($dataUser->result() as $key => $data) {
+            $dataLogin = $this->kunjungan_m->getByDate(date("Y"),date("m"),date("d"),$data->id);
+            $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin","user_id",$data->id,date("Y-m-d"));
+            if ($dataLogin->num_rows() == null and $dataIzin->num_rows() == null) {
+                $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nHalo, ".$data->nama.$data->hp." Terhitung pada ".date("d-m-Y H:i:s")." kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\n_Sistem Otomatis oleh TIM IT UPT_";
+                $this->wa->send($data->hp,$kalimat);
+                sleep(4); // this should halt for 3 seconds for every loop
+                ob_flush();
+                flush();            
             }
         }
     }
