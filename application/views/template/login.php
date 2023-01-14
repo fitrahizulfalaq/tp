cda68e578aafa3e86ab1e5500418624d99455140
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-01-14 21:52:17 +0700

    pwa script

--- application/views/template/login.php
+++ application/views/template/login.php
@@ -44,11 +44,70 @@
     <!-- * loader -->
 
     <!-- App Capsule -->
-    <?=$contents?>
+    <?= $contents ?>
     <!-- * App Capsule -->
 
 
     <!-- ///////////// Js Files ////////////////////  -->
+    <script>
+        let installEvent = null;
+        let installButton = document.getElementById("install");
+        let enableButton = document.getElementById("enable");
+
+        enableButton.addEventListener("click", function() {
+            this.disabled = true;
+            startPwa(true);
+        });
+
+        if (localStorage["pwa-enabled"]) {
+            startPwa();
+        }
+
+        function startPwa(firstStart) {
+            localStorage["pwa-enabled"] = true;
+
+            if (firstStart) {
+                location.reload();
+            }
+
+            window.addEventListener("load", () => {
+                navigator.serviceWorker.register("/service-worker.js")
+                    .then(registration => {
+                        console.log("Service Worker is registered", registration);
+                        enableButton.parentNode.remove();
+                    })
+                    .catch(err => {
+                        console.error("Registration failed:", err);
+                    });
+            });
+
+            window.addEventListener("beforeinstallprompt", (e) => {
+                e.preventDefault();
+                console.log("Ready to install...");
+                installEvent = e;
+                document.getElementById("install").style.display = "initial";
+            });
+
+            setTimeout(cacheLinks, 500);
+
+            function cacheLinks() {
+                caches.open("pwa").then(function(cache) {
+                    let linksFound = [];
+                    document.querySelectorAll("a").forEach(function(a) {
+                        linksFound.push(a.href);
+                    });
+
+                    cache.addAll(linksFound);
+                });
+            }
+
+            if (installButton) {
+                installButton.addEventListener("click", function() {
+                    installEvent.prompt();
+                });
+            }
+        }
+    </script>
     <!-- Jquery -->
     <script src="<?= base_url() ?>/assets/js/lib/jquery-3.4.1.min.js"></script>
     <!-- Bootstrap-->
