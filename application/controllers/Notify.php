7329c0a268bb92c088a7f69097aa243e95c3a5a9
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-01-19 11:00:00 +0700

    broadcast pesan

--- application/controllers/Notify.php
+++ application/controllers/Notify.php
@@ -47,7 +47,7 @@ function __construct(){
             $dataLogin = $this->kunjungan_m->getByDate(date("Y"),date("m"),date("d"),$data->id);
             $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin","user_id",$data->id,date("Y-m-d"));
             if ($dataLogin->num_rows() == null and $dataIzin->num_rows() == null) {
-                sleep(1); // this should halt for 3 seconds for every loop
+                sleep(2); // this should halt for 3 seconds for every loop
                 $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nHalo, ".$data->nama." Terhitung pada ".date("d-m-Y H:i:s")." kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\n_Sistem Otomatis oleh TIM IT UPT_";
                 echo $kalimat . "</br>";
                 // $this->wa->send($data->hp,$kalimat);
