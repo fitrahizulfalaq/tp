f6427b1d6c348b172b1cf62704af2e65def5f509
Fitrah Izul Falaq <fitrahizulfalaq@gmail.com>
2023-02-08 15:20:20 +0700

    tambah notifikasi perizinan ke mbak uun dan mbak indah

--- application/controllers/Notify.php
+++ application/controllers/Notify.php
@@ -1,14 +1,19 @@
 <?php
+
+use Monolog\Handler\IFTTTHandler;
+
 ob_start();
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Notify extends CI_Controller {
+defined('BASEPATH') or exit('No direct script access allowed');
+class Notify extends CI_Controller
+{
 
-function __construct(){
+    function __construct()
+    {
         parent::__construct();
         $this->load->library("wa");
     }
- 
+
     function index()
     {
         redirect("");
@@ -19,33 +24,32 @@ function __construct(){
         // redirect();
         $this->fungsi->saveAdminLog("Simpan Telat");
 
-        if (date("H") < $this->fungsi->setting("jamTelat")->row("value"))
-        {
+        if (date("H") < $this->fungsi->setting("jamTelat")->row("value")) {
             $this->session->set_flashdata('danger', 'Waktu penyimpanan yang telat belum dimulai');
             redirect();
         }
 
         $token = $_GET['token'];
-        if ($token != sha1($this->fungsi->setting("token")->row("value"))){
-            $this->session->set_flashdata('danger', 'Ayolah ' . $this->input->ip_address() .' jangan di hit lagi dong.');
+        if ($token != sha1($this->fungsi->setting("token")->row("value"))) {
+            $this->session->set_flashdata('danger', 'Ayolah ' . $this->input->ip_address() . ' jangan di hit lagi dong.');
             redirect();
         }
-        
-        $cekInput = $this->fungsi->loadDataLike1("tb_late","created",date("Y")."-".date("m")."-".date("d"));
-        if ($cekInput->num_rows() > 0 ){
+
+        $cekInput = $this->fungsi->loadDataLike1("tb_late", "created", date("Y") . "-" . date("m") . "-" . date("d"));
+        if ($cekInput->num_rows() > 0) {
             $this->session->set_flashdata('danger', 'Catatan telat sudah di Input');
             redirect("laporan/listDaerah");
         }
 
         $this->load->model("user_m");
         $this->load->model("kunjungan_m");
-        $dataUser = $this->user_m->getAllby("tipe_user","1");
+        $dataUser = $this->user_m->getAllby("tipe_user", "1");
         foreach ($dataUser->result() as $key => $data) {
-            $dataLogin = $this->kunjungan_m->getByDate(date("Y"),date("m"),date("d"),$data->id);
-            $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin","user_id",$data->id,date("Y-m-d"));
+            $dataLogin = $this->kunjungan_m->getByDate(date("Y"), date("m"), date("d"), $data->id);
+            $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin", "user_id", $data->id, date("Y-m-d"));
             if ($dataLogin->num_rows() == null and $dataIzin->num_rows() == null) {
                 echo $data->nama . " Telat melakukan checkin pada " . date("d - m - Y H : i : s") . "<br>";
-                $this->kunjungan_m->saveLate($data->id,"telat check in");
+                $this->kunjungan_m->saveLate($data->id, "telat check in");
             }
         }
     }
@@ -55,28 +59,27 @@ function __construct(){
         // redirect();
         $this->fungsi->saveAdminLog("Chat Telat");
 
-        if (date("H") < $this->fungsi->setting("jamWa")->row("value"))
-        {
+        if (date("H") < $this->fungsi->setting("jamWa")->row("value")) {
             $this->session->set_flashdata('danger', 'Waktu penyimpanan yang telat belum dimulai');
             redirect();
         }
 
         $token = $_GET['token'];
-        if ($token != sha1($this->fungsi->setting("token")->row("value"))){
-            $this->session->set_flashdata('danger', 'Ayolah ' . $this->input->ip_address() .' jangan di hit lagi dong.');
+        if ($token != sha1($this->fungsi->setting("token")->row("value"))) {
+            $this->session->set_flashdata('danger', 'Ayolah ' . $this->input->ip_address() . ' jangan di hit lagi dong.');
             redirect();
         }
-        
+
         $this->load->model("user_m");
         $this->load->model("kunjungan_m");
-        $dataUser = $this->user_m->getAllby("tipe_user","1");
+        $dataUser = $this->user_m->getAllby("tipe_user", "1");
         foreach ($dataUser->result() as $key => $data) {
-            $dataLogin = $this->kunjungan_m->getByDate(date("Y"),date("m"),date("d"),$data->id);
-            $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin","user_id",$data->id,date("Y-m-d"));
+            $dataLogin = $this->kunjungan_m->getByDate(date("Y"), date("m"), date("d"), $data->id);
+            $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin", "user_id", $data->id, date("Y-m-d"));
             if ($dataLogin->num_rows() == null and $dataIzin->num_rows() == null) {
-                $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nHalo, ".$data->nama." Terhitung pada ".date("d-m-Y H:i:s")." kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\n_Sistem Otomatis oleh TIM IT UPT_";
+                $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nHalo, " . $data->nama . " Terhitung pada " . date("d-m-Y H:i:s") . " kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\n_Sistem Otomatis oleh TIM IT UPT_";
                 // echo $kalimat . "</br>";
-                $this->wa->send($data->hp,$kalimat);
+                $this->wa->send($data->hp, $kalimat);
                 sleep(0.7); // this should halt for 3 seconds for every loop
             }
             ob_flush();
@@ -89,16 +92,16 @@ function __construct(){
         $this->fungsi->saveAdminLog("Checking");
 
         $token = $_GET['token'];
-        if ($token != "3847de719aa1d918d17dbd1f54193873e8f6f317"){
-            $this->session->set_flashdata('danger', 'Ayolah ' . $this->input->ip_address() .' jangan di hit lagi dong.');
+        if ($token != "3847de719aa1d918d17dbd1f54193873e8f6f317") {
+            $this->session->set_flashdata('danger', 'Ayolah ' . $this->input->ip_address() . ' jangan di hit lagi dong.');
             redirect();
         }
         $this->load->model("user_m");
         $this->load->model("kunjungan_m");
-        $dataUser = $this->user_m->getAllby("tipe_user","1");
+        $dataUser = $this->user_m->getAllby("tipe_user", "1");
         foreach ($dataUser->result() as $key => $data) {
             sleep(1); // this should halt for 3 seconds for every loop
-            $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nHalo, ".$data->nama." Terhitung pada ".date("d-m-Y H:i:s")." kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\n_Sistem Otomatis oleh TIM IT UPT_";
+            $kalimat = "*[KAMU BELUM CHECK IN KUNJUNGAN HARI INI]* \n\nHalo, " . $data->nama . " Terhitung pada " . date("d-m-Y H:i:s") . " kamu belum melakukan checkin kunjungan. Segera melaporkan kegiatan hari ini, terima kasih. \n\n\n https://tp.upktukm.id\n_Sistem Otomatis oleh TIM IT UPT_";
             echo $kalimat . "</br>";
             ob_flush();
             flush();
@@ -120,24 +123,27 @@ function __construct(){
         $this->fungsi->saveAdminLog("WA Fitrah");
 
         $token = $_GET['token'];
-        if ($token != "3847de719aa1d918d17dbd1f54193873e8f6f317"){
-            $this->session->set_flashdata('danger', 'Ayolah ' . $this->input->ip_address() .' jangan di hit lagi dong.');
+        if ($token != "3847de719aa1d918d17dbd1f54193873e8f6f317") {
+            $this->session->set_flashdata('danger', 'Ayolah ' . $this->input->ip_address() . ' jangan di hit lagi dong.');
             redirect();
         }
         $this->load->model("user_m");
         $this->load->model("kunjungan_m");
-        $dataUser = $this->user_m->getAllby("tipe_user","1");
+        $dataUser = $this->user_m->getAllby("tipe_user", "1");
         $no = 1;
         $pengguna = "";
         foreach ($dataUser->result() as $key => $data) {
-            $dataLogin = $this->kunjungan_m->getByDate(date("Y"),date("m"),date("d"),$data->id);
-            $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin","user_id",$data->id,date("Y-m-d"));
+            $dataLogin = $this->kunjungan_m->getByDate(date("Y"), date("m"), date("d"), $data->id);
+            $dataIzin = $this->kunjungan_m->getAllByTable("tb_izin", "user_id", $data->id, date("Y-m-d"));
             if ($dataLogin->num_rows() == null and $dataIzin->num_rows() == null) {
-                $pengguna = $pengguna . $no++ . ". " . $data->nama." Belum login pada ".date("d-m-Y H:i:s")."\n";
+                $pengguna = $pengguna . $no++ . ". " . $data->nama . " Belum login pada " . date("d-m-Y H:i:s") . "\n";
             }
         }
-        $kalimat = "*[LIST YANG BELUM CHECK IN KUNJUNGAN HARI INI]*\n\n".$pengguna;
-        $this->wa->send("081231390340",$kalimat);
+        $kalimat = "*[LIST YANG BELUM CHECK IN KUNJUNGAN HARI INI]*\n\n" . $pengguna;
+        $this->wa->send("081231390340", $kalimat);
+        if (date("H") < "18") {
+            $this->wa->send("085235142348", $kalimat);
+            $this->wa->send("085746706296", $kalimat);
+        }
     }
-
 }
